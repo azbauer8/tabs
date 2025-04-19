04ccbe010426c136f18b53a9ed3fafd11fe8641c
Zach Bauer <azbauer8@gmail.com>
2025-04-19 15:17:49 -0400

    testing siteUrl change

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,10 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const siteUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+export const siteUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://tabs.byzach.dev";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
