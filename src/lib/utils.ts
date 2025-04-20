4d299af6711eaa300ea36c11ad127e0f4a934a06
Zach Bauer <azbauer8@gmail.com>
2025-04-20 19:05:55 -0400

    testing using different vercel env var for production url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const siteUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+export const siteUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : "http://localhost:3000";
 
 export function cn(...inputs: ClassValue[]) {
